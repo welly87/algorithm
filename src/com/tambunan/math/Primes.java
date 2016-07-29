1c3822ce93d0cc619db386d3c0bdd2bbc5725a38
Welly Tambunan <if05041@gmail.com>
2016-07-29 15:28:52 +0700

    adding shortest path algorithm

---
+++ src/com/tambunan/math/Primes.java
@@ -0,0 +1,21 @@
+package com.tambunan.math;
+
+/**
+ * Created by Welly on 7/29/2016.
+ */
+public class Primes {
+    public boolean isPrime (int n)
+    {
+        if (n<=1) return false;
+        if (n==2) return true;
+        if (n%2==0) return false;
+        int m= (int)Math.sqrt(n);
+
+        for (int i=3; i<=m; i+=2)
+            if (n%i==0)
+                return false;
+
+        return true;
+    }
+
+}

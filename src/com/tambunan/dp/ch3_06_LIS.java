b4561e61a869892299ef6973c19b996b2da7cef5
Welly Tambunan <if05041@gmail.com>
2016-08-05 20:02:54 +0700

    adding longest increasing sub bottom up with solution

--- src/com/tambunan/dp/ch3_06_LIS.java
+++ src/com/tambunan/dp/ch3_06_LIS.java
@@ -73,7 +73,7 @@ class LongestIncreasingSub {
 
         if (memo[i] != -1) return memo[i];
 
-        int max = -1;
+        int max = 0;
 
         for (int j = 0; j < i; j++) {
             if (A[j] < A[i]) {
@@ -122,6 +122,59 @@ class LongestDecreasingSub {
     }
 }
 
+// next bottom up dp
+
+class LongestIncreasingSubBottomUp {
+    public int solve(int arr[]) {
+        int T[] = new int[arr.length];
+        int actualSolution[] = new int[arr.length];
+
+        for (int i = 0; i < arr.length; i++) {
+            T[i] = 1;
+            actualSolution[i] = i;
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < i; j++) {
+                if (arr[i] > arr[j]) {
+                    if (T[j] + 1 > T[i]) {
+                        T[i] = T[j] + 1;
+                        actualSolution[i] = j;
+                    }
+                }
+            }
+        }
+
+        int maxIndex = 0;
+
+        for (int i = 0; i < T.length; i++) {
+            if (T[i] > T[maxIndex]) {
+                maxIndex = i;
+            }
+        }
+
+        int t = maxIndex;
+        int newT = maxIndex;
+        int[] solution = new int[T[maxIndex]];
+        int idx = T[maxIndex];
+
+        do {
+            t = newT;
+            solution[--idx] = arr[t];
+            // System.out.print(arr[t] + " ");
+            newT = actualSolution[t];
+        } while (t != newT);
+        //System.out.println();
+
+        for (int sl : solution) {
+            System.out.print(sl + " ");
+        }
+        System.out.println();
+
+        return T[maxIndex];
+    }
+}
+
 class ch3_06_LIS {
 
 
@@ -131,13 +184,18 @@ class ch3_06_LIS {
         int n = 11;
         int[] A = new int[]{-7, 10, 9, 2, 3, 8, 8, 1, 2, 3, 4};
 
+
+        LongestIncreasingSubBottomUp a = new LongestIncreasingSubBottomUp();
+
+        System.out.println(a.solve(A));
+
 //        LongestIncreasingSubsequence a = new LongestIncreasingSubsequence();
 //
 //        System.out.printf("Final LIS is of length %d: ", a.solve(A).length);
 
-        System.out.println(new LongestIncreasingSub().LIS(5));
-        System.out.println(new LongestDecreasingSub().longest());
-
-        System.out.println(new LongestDecreasingSub().LDS(1));
+//        System.out.println(new LongestIncreasingSub().LIS(5));
+//        System.out.println(new LongestDecreasingSub().longest());
+//
+//        System.out.println(new LongestDecreasingSub().LDS(1));
     }
 }
